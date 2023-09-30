c27c86690d4050ea9a460dc4e094da9f866f85f7
Masamune <bronnikov_aleks@mail.ru>
2023-09-30 20:51:21 +0900

    Added Neuro Hunter, my web game resurrection.

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+/*
+        '../images/screenshots/bio/bio-colony-center.jpg',
+        '../images/screenshots/bio/bio-geo-lab.jpg',
+        '../images/screenshots/bio/bio-greenhouses.jpg',
+        '../images/screenshots/bio/bio-prison-cell.jpg',
+        '../images/screenshots/bio/bio-rezector.jpg',
+        '../images/screenshots/bio/bio-shooting-turret.jpg',
+        '../images/screenshots/bio/bio-strange-cave.jpg',
+        '../images/screenshots/bio/bio-tunnel-monster.jpg'
+        // Добавьте пути к остальным картинкам
+    ];
+
+    var randomIndex = Math.floor(Math.random() * images.length);
+    var randomImage = images[randomIndex];
+
+    var imgElement = document.getElementById('randomImage');
+    imgElement.src = randomImage;
+})(); */
+
